88ace951e008e599f2dcb8f4eb90ff6f6537fc37
NickVeles <xm1k123x@gmail.com>
2025-02-28 19:17:21 +0100

    add style prop to ParallaxScrollView for customizable styling

--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren, ReactElement } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ViewStyle } from 'react-native';
 import Animated, {
   interpolate,
   useAnimatedRef,
@@ -16,12 +16,14 @@ const HEADER_HEIGHT = 250;
 type Props = PropsWithChildren<{
   headerImage?: ReactElement;
   headerBackgroundColor: { dark: string; light: string };
+  style?: ViewStyle;
 }>;
 
 export default function ParallaxScrollView({
   children,
   headerImage,
   headerBackgroundColor,
+  style,
 }: Props) {
   const colorScheme = useColorScheme() ?? 'light';
   const scrollRef = useAnimatedRef<Animated.ScrollView>();
@@ -45,7 +47,7 @@ export default function ParallaxScrollView({
   });
 
   return (
-    <ThemedView style={styles.container}>
+    <ThemedView style={[styles.container, style]}>
       <Animated.ScrollView
         ref={scrollRef}
         scrollEventThrottle={16}
