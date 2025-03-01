cb55f5d9be7adc04e976fe9cbd7e0862405e48ea
NickVeles <xm1k123x@gmail.com>
2025-03-01 11:04:44 +0100

    add EditIcon component with default size and stroke properties

--- components/ui/Icon.tsx
+++ components/ui/Icon.tsx
@@ -137,6 +137,26 @@ export const BackIcon = ({ height, width, stroke, strokeWidth }: SvgComponentPro
   </Svg>
 )
 
+export const EditIcon = ({ height, width, stroke, strokeWidth }: SvgComponentProps, props: any) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width ?? height ?? DEFAULT_SIZE}
+    height={height ?? width ?? DEFAULT_SIZE}
+    viewBox="0 0 24 24"
+    stroke={stroke ?? DEFAULT_STROKE}
+    strokeWidth={strokeWidth ?? DEFAULT_STROKE_WIDTH}
+    fill="none"
+    alt="Edit"
+    {...props}
+  >
+    <Path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+    />
+  </Svg>
+)
+
 export const CheckIcon = ({ height, width, stroke, strokeWidth }: SvgComponentProps, props: any) => (
   <Svg
     xmlns="http://www.w3.org/2000/svg"
