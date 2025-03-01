534c0a90e6d33cc7ec50a7a198bd7a36249394fb
NickVeles <xm1k123x@gmail.com>
2025-03-01 15:31:35 +0100

    feat: enhance ConfirmModal with improved cancel button styling and animation

--- components/ConfirmModal.tsx
+++ components/ConfirmModal.tsx
@@ -36,18 +36,18 @@ export const ConfirmModal: React.FC<ConfirmModalProps> = ({
   };
 
   return (
-    <Modal transparent visible={isVisible} animationType="fade">
+    <Modal transparent visible={isVisible} animationType="none">
       <TouchableOpacity style={styles.overlay} onPress={handleClose} activeOpacity={1}>
         <View style={styles.modal}>
           <Text style={styles.title}>{title}</Text>
           <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.button} onPress={handleClose}>
-              <XMarkIcon height={24} width={24} stroke="#121212" />
-              {cancelText && <Text style={styles.buttonText}>{cancelText}</Text>}
-            </TouchableOpacity>
             <TouchableOpacity style={styles.button} onPress={handleConfirm}>
               <CheckIcon height={24} width={24} stroke="#121212" />
               {acceptText && <Text style={styles.buttonText}>{acceptText}</Text>}
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, styles.cancelButton]} onPress={handleClose}>
+              <XMarkIcon height={24} width={24} stroke="#f2f2f2" />
+              {cancelText && <Text style={[styles.buttonText, { color: '#f2f2f2' }]}>{cancelText}</Text>}
             </TouchableOpacity>
           </View>
         </View>
@@ -72,6 +72,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   title: {
+    color: '#121212',
     fontSize: 18,
     fontWeight: 'bold',
     paddingHorizontal: 10,
@@ -92,6 +93,9 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: 'rgba(75, 75, 75, 0.1)',
     borderRadius: 5,
+  },
+  cancelButton: {
+    backgroundColor: 'rgb(176, 72, 91)',
   },
   buttonText: {
     fontSize: 16,
