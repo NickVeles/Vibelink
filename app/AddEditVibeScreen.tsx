2a01a441015c34521add27c3117a7befd3797cf2
NickVeles <xm1k123x@gmail.com>
2025-03-06 15:28:41 +0100

    feat: add emoji picker and validation to AddEditVibeScreen

--- app/AddEditVibeScreen.tsx
+++ app/AddEditVibeScreen.tsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { View, TextInput, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import {
+  View,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+} from 'react-native';
 import { useRouter } from 'expo-router';
 import { Vibe } from '@/models/Vibe';
 import Header from '@/components/Header';
@@ -9,6 +15,7 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ConfirmModal } from '@/components/ConfirmModal';
 import FloatingButton from '@/components/ui/FloatingButton';
 import Color from 'color';
+import EmojiPicker, { type EmojiType } from 'rn-emoji-keyboard';
 
 interface EditVibeScreenProps {
   vibe: Vibe;
@@ -28,9 +35,43 @@ export default function AddEditVibeScreen({
   const [isConfirmable, setIsConfirmable] = useState(vibe.isConfirmable);
   const [confirmModalVisible, setConfirmModalVisible] = useState(false); // State for ConfirmModal visibility
   const [maxTextWidth, setMaxTextWidth] = useState(0);
+  const [emojiPickerVisible, setEmojiPickerVisible] = useState(false);
+
+  const [isTextError, setIsTextError] = useState(false);
+  const [textError, setTextError] = useState('');
+  const [isEmojiError, setIsEmojiError] = useState(false);
+  const [isSaveButtonDisabled, setIsSaveButtonDisabled] = useState(false);
 
   const closeConfirmModal = () => {
     setConfirmModalVisible(false);
+  };
+
+  useEffect(() => {
+    setIsSaveButtonDisabled(isTextError || isEmojiError);
+  }, [isTextError, isEmojiError]);
+
+  const handleChangeText = (value: string) => {
+    setText(value);
+
+    // Check for text length
+    if (text.length > 16) {
+      setIsTextError(true);
+      setTextError('Your vibe can have at most 16 characters');
+    } else if (text.length == 0) {
+      setIsTextError(true);
+      setTextError('Your vibe cannot be empty');
+    } else {
+      setIsTextError(false);
+      setTextError('');
+    }
+  };
+
+  const handleEmojiSelected = (emojiObject: EmojiType) => {
+    setEmoji(emojiObject.emoji);
+    setEmojiPickerVisible(false);
+
+    // Check for empty emoji
+    setIsEmojiError(emoji == '');
   };
 
   const handleLeave = async () => {
@@ -51,7 +92,13 @@ export default function AddEditVibeScreen({
   };
 
   const handleSave = async () => {
-    const updatedVibe = { ...vibe, text, emoji, color, isConfirmable };
+    const updatedVibe = {
+      id: vibe.id,
+      text: text.trim(),
+      emoji: emoji,
+      color: color,
+      isConfirmable: isConfirmable,
+    };
     onSave(updatedVibe);
     router.back();
   };
@@ -67,7 +114,11 @@ export default function AddEditVibeScreen({
       <ParallaxScrollView>
         {/* Preview */}
         <LinearGradient
-          colors={[Color(color).rotate(8).hex(), color, Color(color).rotate(-8).hex()]}
+          colors={[
+            Color(color).rotate(8).hex(),
+            color,
+            Color(color).rotate(-8).hex(),
+          ]}
           start={[1, 0]}
           end={[0, 0.75]}
           style={styles.vibeButton}
@@ -75,26 +126,64 @@ export default function AddEditVibeScreen({
             // make space for the emojis
             const { width } = event.nativeEvent.layout;
             setMaxTextWidth(width - 128);
-          }}>
-            {/* Button Content */}
-            <Text style={styles.emoji}>
-              {emoji}
-            </Text>
-            <Text style={[styles.vibeText, { maxWidth: maxTextWidth }]}>
-              {text}
-            </Text>
-            <Text style={[styles.emoji, { opacity: 0 }]}>
-              {emoji}
-            </Text>
+          }}
+        >
+          {/* Button Content */}
+          <Text style={styles.emoji}>{emoji}</Text>
+          <Text style={[styles.vibeText, { maxWidth: maxTextWidth }]}>
+            {text}
+          </Text>
+          <Text style={[styles.emoji, { opacity: 0 }]}>{emoji}</Text>
         </LinearGradient>
+
+        {/* Text */}
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>What's your vibe?</Text>
+          <TextInput
+            value={text}
+            onChangeText={handleChangeText}
+            placeholder="e.g. Happy"
+            style={[
+              styles.input,
+              { borderColor: isTextError ? '#b0485b' : '#424242' },
+            ]}
+          />
+          {isTextError && (
+            <Text style={[styles.label, { color: '#b0485b' }]}>
+              {textError}
+            </Text>
+          )}
+        </View>
+
+        {/* Emoji */}
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Pick an emoji</Text>
+          <TouchableOpacity
+            style={styles.input}
+            onPress={() => setEmojiPickerVisible(true)}
+          >
+            <Text style={styles.emoji}>{emoji}</Text>
+          </TouchableOpacity>
+          <EmojiPicker
+            onEmojiSelected={handleEmojiSelected}
+            open={emojiPickerVisible}
+            onClose={() => setEmojiPickerVisible(false)}
+          />
+          {isEmojiError && (
+            <Text style={[styles.label, { color: '#b0485b' }]}>
+              You have to pick an emoji
+            </Text>
+          )}
+        </View>
       </ParallaxScrollView>
 
       {/* Save button */}
-      <FloatingButton 
+      <FloatingButton
         onPress={handleSave}
         color1="#3d9f3c"
         color2="#1b6e13"
         content={<CheckIcon height={50} width={50} />}
+        disabled={isSaveButtonDisabled}
       />
 
       {/* Confirm modal */}
@@ -149,5 +238,18 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 0 },
     shadowRadius: 10,
     elevation: 5,
+  },
+  inputContainer: {
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+  label: {
+    fontSize: 12,
+    color: '#424242',
+  },
+  input: {
+    padding: 4,
+    borderRadius: 5,
+    borderWidth: 1,
   },
 });
