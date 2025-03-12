0d7ccc5a00f8d1d7a63c2371e81f18ef0019506b
NickVeles <xm1k123x@gmail.com>
2025-03-12 15:53:07 +0100

    feat: settings screen general improvements

--- app/settings.tsx
+++ app/settings.tsx
@@ -2,17 +2,29 @@ import DataContext from '@/components/DataContext';
 import Header from '@/components/Header';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { BackIcon } from '@/components/ui/Icon';
+import { LinearGradient } from 'expo-linear-gradient';
 import { useRouter } from 'expo-router';
-import { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { useContext, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function SettingsScreen() {
   const router = useRouter();
   const dataContext = useContext(DataContext);
   const settings = dataContext?.settings;
 
+  const [isShowCustomConnection, setIsShowCustomConnection] = useState(true);
+
   const handleLeave = () => {
     router.back();
+  };
+
+  const switchCustomConnection = () => {
+    if (!settings) return;
+
+    dataContext.updateSettings({
+      ...settings,
+      isCustomConnection: !settings.isCustomConnection,
+    })
   };
 
   return (
@@ -24,12 +36,77 @@ export default function SettingsScreen() {
 
       {/* Content */}
       <ParallaxScrollView>
-        {dataContext && (
+        {settings && (
           <>
-            <View></View>
+            {/* isCustomConnection? */}
+            <View style={styles.inputContainer}>
+              <Text style={styles.label}>Ask me before sending this vibe</Text>
+              <TouchableOpacity
+                activeOpacity={1}
+                style={[styles.input, { flexDirection: 'row' }]}
+                onPress={switchCustomConnection}
+              >
+                <LinearGradient
+                  colors={
+                    !settings?.customConnection
+                      ? ['#b0485b', '#ff6f61']
+                      : ['#f9f9f9', '#f9f9f9']
+                  }
+                  start={[1, 0]}
+                  end={[0, 0.75]}
+                  style={styles.inputButton}
+                >
+                  <Text
+                    style={[
+                      styles.inputText,
+                      {
+                        color: !settings?.customConnection
+                          ? '#f9f9f9'
+                          : '#121212',
+                      },
+                    ]}
+                  >
+                    Off
+                  </Text>
+                </LinearGradient>
+                <LinearGradient
+                  colors={
+                    settings?.customConnection
+                      ? ['#b0485b', '#ff6f61']
+                      : ['#f9f9f9', '#f9f9f9']
+                  }
+                  start={[0, 0.75]}
+                  end={[1, 0]}
+                  style={[
+                    styles.inputButton,
+                    {
+                      borderRadius: 0,
+                      borderTopRightRadius: 3.5,
+                      borderBottomRightRadius: 3.5,
+                    },
+                  ]}
+                >
+                  <Text
+                    style={[
+                      styles.inputText,
+                      {
+                        color: settings?.customConnection
+                          ? '#f9f9f9'
+                          : '#121212',
+                      },
+                    ]}
+                  >
+                    On
+                  </Text>
+                </LinearGradient>
+              </TouchableOpacity>
+              <View style={[styles.errorLabel]} />
+            </View>
           </>
         )}
-        {!dataContext && (<Text style={styles.loadingText}>Loading vibe...</Text>)}
+        {!settings && (
+          <Text style={styles.loadingText}>Loading vibe...</Text>
+        )}
       </ParallaxScrollView>
     </View>
   );
@@ -40,6 +117,42 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontStyle: 'italic',
     padding: 20,
+    userSelect: 'none',
+  },
+  inputContainer: {
+    paddingTop: 8,
+  },
+  input: {
+    fontSize: 20,
+    borderRadius: 5,
+    borderWidth: 1,
+    height: 45,
+  },
+  inputButton: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+    borderTopLeftRadius: 3.5,
+    borderBottomLeftRadius: 3.5,
+  },
+  inputText: {
+    flex: 1,
+    alignContent: 'center',
+    textAlign: 'center',
+    fontSize: 20,
+    color: '#f2f2f2',
+    userSelect: 'none',
+  },
+  label: {
+    fontSize: 18,
+    color: '#424242',
+    paddingBottom: 2,
+    userSelect: 'none',
+  },
+  errorLabel: {
+    minHeight: 22,
+    fontSize: 16,
+    color: '#b0485b',
     userSelect: 'none',
   },
 });
