598f7fd7efecbc2022140fb5882f0a1726247368
NickVeles <xm1k123x@gmail.com>
2025-03-12 14:11:23 +0100

    feat: add EmojiIcon

--- components/ui/Icon.tsx
+++ components/ui/Icon.tsx
@@ -236,3 +236,23 @@ export const TrashIcon = ({ height, width, stroke, strokeWidth }: SvgComponentPr
     />
   </Svg>
 )
+
+export const EmojiIcon = ({ height, width, stroke, strokeWidth }: SvgComponentProps, props: any) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width ?? height ?? DEFAULT_SIZE}
+    height={height ?? width ?? DEFAULT_SIZE}
+    viewBox="0 0 24 24"
+    stroke={stroke ?? DEFAULT_STROKE}
+    strokeWidth={strokeWidth ?? DEFAULT_STROKE_WIDTH}
+    fill="none"
+    alt="Emoji"
+    {...props}
+  >
+    <Path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z"
+    />
+  </Svg>
+)
