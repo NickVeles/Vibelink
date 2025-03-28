3842227800245257c022e79f9a8dcde5aabfb83f
NickVeles <xm1k123x@gmail.com>
2025-03-28 21:14:03 +0100

    feat: integrate Firebase initialization and add firebaseApp to DataContext

--- components/DataContext.tsx
+++ components/DataContext.tsx
@@ -9,10 +9,27 @@ import {
 import { Vibe } from '@/models/Vibe';
 import { Settings } from '@/models/Settings';
 import { DefaultSettings } from '@/utils/defaults';
+import { initializeApp, FirebaseApp } from 'firebase/app';
+
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '@/.env.local' });
+
+const firebaseContfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+const firebaseApp = initializeApp(firebaseContfig);
 
 interface DataContextType {
   vibes: Vibe[];
   settings: Settings;
+  firebaseApp: FirebaseApp;
   addOrUpdateVibe: (vibe: Vibe) => Promise<void>;
   updateVibes: (vibes: Vibe[]) => Promise<void>;
   updateSettings: (settings: Settings) => Promise<void>;
@@ -56,7 +73,14 @@ export const DataProvider: React.FC<{ children: React.ReactNode }> = ({
 
   return (
     <DataContext.Provider
-      value={{ vibes, settings, addOrUpdateVibe, updateVibes, updateSettings }}
+      value={{
+        vibes,
+        settings,
+        firebaseApp,
+        addOrUpdateVibe,
+        updateVibes,
+        updateSettings,
+      }}
     >
       {children}
     </DataContext.Provider>
