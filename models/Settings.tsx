850ff886ce45bf035823855e736d60363fb517dc
NickVeles <xm1k123x@gmail.com>
2025-03-28 20:21:01 +0100

    refactor: remove unused Firebase connection settings and related logic

--- models/Settings.tsx
+++ models/Settings.tsx
@@ -1,8 +1,5 @@
-import { FirebaseConnection } from "./FirebaseConnection";
-
 export interface Settings {
   isLeftEmojiVisible: boolean,
   isRightEmojiVisible: boolean,
   isCustomConnection: boolean,
-  customConnection?: FirebaseConnection,
 }
