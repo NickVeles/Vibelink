e5f6b89213d665bf4d6175e3d5c0170d5b12fe58
NickVeles <xm1k123x@gmail.com>
2025-04-01 21:41:39 +0200

    feat: add app.config.js for Expo configuration and remove app.json

---
+++ app.config.js
@@ -0,0 +1,54 @@
+// filepath: c:\Code\Vibelink\app.config.js
+
+export default {
+  expo: {
+    name: "Vibelink",
+    slug: "Vibelink",
+    version: "1.0.1",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      firebaseConfig: {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+      },
+    },
+  },
+};
--- components/DataContext.tsx
+++ components/DataContext.tsx
@@ -11,20 +11,18 @@ import { Settings } from '@/models/Settings';
 import { DefaultSettings } from '@/utils/defaults';
 import { initializeApp, FirebaseApp } from 'firebase/app';
 
-import * as dotenv from 'dotenv';
-dotenv.config({ path: '@/.env.local' });
+import Constants from 'expo-constants';
 
-const firebaseContfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+const firebaseConfig = Constants.expoConfig?.extra?.firebaseConfig ?? {
+  apiKey: "dummyApiKey",
+  authDomain: "dummyProjectId.firebaseapp.com",
+  projectId: "dummyProjectId",
+  storageBucket: "dummyProjectId.appspot.com",
+  messagingSenderId: "dummySenderId",
+  appId: "dummyAppId",
 };
 
-const firebaseApp = initializeApp(firebaseContfig);
+const firebaseApp = initializeApp(firebaseConfig);
 
 interface DataContextType {
   vibes: Vibe[];
